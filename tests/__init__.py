7b7f4c907ec953d2386bf79cfec2d4b297255202
rodrigondec <rodrigondec@gmail.com>
2019-10-01 00:31:29 -0300

    base test configured

---
+++ tests/__init__.py
@@ -0,0 +1,15 @@
+from flask_testing import TestCase
+
+from app import app, db
+
+
+class BaseTestCase(TestCase):
+    def create_app(self):
+        return app
+
+    def setUp(self):
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()

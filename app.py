463e6d73394bba9ae15143a8d6f5b0c870eed6a2
rodrigondec <rodrigondec@gmail.com>
2019-10-01 00:02:03 -0300

    app reworked

--- app.py
+++ app.py
@@ -3,20 +3,18 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
-from notifiers.logging import NotificationHandler
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from flasgger import Swagger
-from loguru import logger
 
 from config import configure_app
-from transparencia_api.resources import add_resources
+from resources import add_resources
 
 
 app = Flask(__name__)
 CORS(app)
 configure_app(app)
-api = Api(app, prefix='/api/v1')
+api = Api(app)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 migrate = Migrate(app, db)
@@ -25,3 +23,4 @@ swagger = Swagger(app)
 
 
 add_resources(api)
+from models import *

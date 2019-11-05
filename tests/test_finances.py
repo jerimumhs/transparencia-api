49da01fa867e07012806dbec618da32f766974a5
rodrigondec <rodrigondec@gmail.com>
2019-11-05 16:40:18 -0300

    test finances created

--- tests/test_finances.py
+++ tests/test_finances.py
@@ -1,22 +1,36 @@
 from tests import BaseTestCase
-from models.finances import Ticket, Income, Expense
+from models.finances import Ticket
+from tests.factories.finances import TicketFactory
 
 
 class TestFinances(BaseTestCase):
-    def test_ticket(self):
+    def test_create_ticket(self):
         self.assertEqual(Ticket.query.count(), 0)
-        Ticket(name='Teste', value=10.50).save()
+        TicketFactory()
         self.assertEqual(Ticket.query.count(), 1)
-        self.assertEqual(Ticket.query.first().name, 'Teste')
 
-    def test_income(self):
-        self.assertEqual(Income.query.count(), 0)
-        Income(name='Teste', value=10.50).save()
-        self.assertEqual(Income.query.count(), 1)
-        self.assertEqual(Income.query.first().name, 'Teste')
+    def test_create_ticket_fail(self):
+        self.assertEqual(Ticket.query.count(), 0)
+        self.assertRaises(ValueError, TicketFactory, name='')
+        self.assertRaises(ValueError, TicketFactory, name=None)
+        self.assertRaises(ValueError, TicketFactory, date=None)
+        self.assertRaises(ValueError, TicketFactory, value=None)
+        self.assertRaises(ValueError, TicketFactory, value=0)
+        self.assertRaises(ValueError, TicketFactory, value=-1)
+        self.assertRaises(ValueError, TicketFactory, type='')
+        self.assertRaises(ValueError, TicketFactory, type='TEASER')
+        self.assertEqual(Ticket.query.count(), 0)
 
-    def test_expense(self):
-        self.assertEqual(Expense.query.count(), 0)
-        Expense(name='Teste', value=10.50).save()
-        self.assertEqual(Expense.query.count(), 1)
-        self.assertEqual(Expense.query.first().name, 'Teste')
+    def test_create_income(self):
+        self.assertEqual(Ticket.query.count(), 0)
+        TicketFactory(type=Ticket.INCOME)
+        self.assertEqual(Ticket.query.count(), 1)
+        self.assertEqual(Ticket.incomes.count(), 1)
+        self.assertEqual(Ticket.expenses.count(), 0)
+
+    def test_create_expense(self):
+        self.assertEqual(Ticket.query.count(), 0)
+        TicketFactory(type=Ticket.EXPENSE)
+        self.assertEqual(Ticket.query.count(), 1)
+        self.assertEqual(Ticket.expenses.count(), 1)
+        self.assertEqual(Ticket.incomes.count(), 0)

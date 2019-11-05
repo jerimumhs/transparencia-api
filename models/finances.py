fec88a38eb949a8fc16b5e0c9d581d65a936c8cf
rodrigondec <rodrigondec@gmail.com>
2019-11-05 16:39:44 -0300

    finances reworked

--- models/finances.py
+++ models/finances.py
@@ -1,30 +1,55 @@
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy_utils import ChoiceType
+from sqlalchemy.orm import validates
 
 from models.base import db, BaseModel
+from utils import ClassProperty
 
 
 class Ticket(BaseModel):
-    name = db.Column(db.String(50))
-    value = db.Column(db.Float)
+    EXPENSE = 'e'
+    INCOME = 'i'
+    TYPES = [
+        (EXPENSE, 'Gasto'),
+        (INCOME, 'Renda')
+    ]
 
-    type = db.Column(db.String(10))
-    __mapper_args__ = {
-        'polymorphic_identity': 'ticket',
-        'polymorphic_on': type
-    }
+    name = db.Column(db.String(50), nullable=False)
+    value = db.Column(db.Float, nullable=False)
+    date = db.Column(db.Date, nullable=False)
 
+    _type = db.Column(ChoiceType(TYPES), nullable=False)
 
-class Expense(Ticket):
-    id = db.Column(UUID(as_uuid=True), db.ForeignKey('ticket.id'), primary_key=True)
+    @validates('name', 'value', 'date', '_type')
+    def validate_fields(self, key, value):
+        if value in [None, '']:
+            raise ValueError(f'O atributo {key} precisa ter um valor atribuido')
 
-    __mapper_args__ = {
-        'polymorphic_identity': 'expense',
-    }
+        if key == 'value':
+            self.validate_value(key, value)
 
+        return value
 
-class Income(Ticket):
-    id = db.Column(UUID(as_uuid=True), db.ForeignKey('ticket.id'), primary_key=True)
+    @staticmethod
+    def validate_value(key,  value):
+        if value <= 0:
+            raise ValueError(f'O atributo {key} precisa ter um valor positivo')
 
-    __mapper_args__ = {
-        'polymorphic_identity': 'income',
-    }
+    @property
+    def type(self):
+        return self._type.value
+
+    @type.setter
+    def type(self, value):
+        if value not in [t[0] for t in self.TYPES]:
+            raise ValueError(f'Os possíveis valores para o type são: {[t[0] for t in self.TYPES]}')
+        self._type = value
+
+    # noinspection PyMethodParameters
+    @ClassProperty
+    def expenses(cls):
+        return cls.query.filter_by(_type=cls.EXPENSE)
+
+    # noinspection PyMethodParameters
+    @ClassProperty
+    def incomes(cls):
+        return cls.query.filter_by(_type=cls.INCOME)

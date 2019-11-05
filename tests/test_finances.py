5d44eb5b15ff531e532d46b3ed4c6bd15b5b6ccd
rodrigondec <rodrigondec@gmail.com>
2019-11-05 15:18:03 -0300

    Test finances ok

--- tests/test_finances.py
+++ tests/test_finances.py
@@ -1,10 +1,22 @@
 from tests import BaseTestCase
-from models.finances import Ticket
+from models.finances import Ticket, Income, Expense
 
 
-class TestExpenses(BaseTestCase):
+class TestFinances(BaseTestCase):
     def test_ticket(self):
         self.assertEqual(Ticket.query.count(), 0)
         Ticket(name='Teste', value=10.50).save()
         self.assertEqual(Ticket.query.count(), 1)
         self.assertEqual(Ticket.query.first().name, 'Teste')
+
+    def test_income(self):
+        self.assertEqual(Income.query.count(), 0)
+        Income(name='Teste', value=10.50).save()
+        self.assertEqual(Income.query.count(), 1)
+        self.assertEqual(Income.query.first().name, 'Teste')
+
+    def test_expense(self):
+        self.assertEqual(Expense.query.count(), 0)
+        Expense(name='Teste', value=10.50).save()
+        self.assertEqual(Expense.query.count(), 1)
+        self.assertEqual(Expense.query.first().name, 'Teste')

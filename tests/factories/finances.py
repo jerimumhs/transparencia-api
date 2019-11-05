0396cca736d163baa87e06334608c889c51d0da4
rodrigondec <rodrigondec@gmail.com>
2019-11-05 16:40:04 -0300

    TicketFactory created

---
+++ tests/factories/finances.py
@@ -0,0 +1,14 @@
+from factory import Faker
+
+from models.finances import Ticket
+from tests.factories import BaseModelFactory
+
+
+class TicketFactory(BaseModelFactory):
+    class Meta:
+        model = Ticket
+
+    name = Faker('text', max_nb_chars=14)
+    value = Faker('pyfloat', positive=True, right_digits=2, max_value=100, min_value=1)
+    date = Faker('date_this_year')
+    type = Faker('random_element', elements=[t[0] for t in Ticket.TYPES])

36cf61900b6bdb7c389f77254463cf795c989709
rodrigondec <rodrigondec@gmail.com>
2019-11-04 21:37:57 -0300

    test ticket creation

--- tests/test_finances.py
+++ tests/test_finances.py
@@ -1,6 +1,10 @@
 from tests import BaseTestCase
+from models.finances import Ticket
 
 
 class TestExpenses(BaseTestCase):
-    def test_example(self):
-        assert 1 == 1
+    def test_ticket(self):
+        self.assertEqual(Ticket.query.count(), 0)
+        Ticket(name='Teste', value=10.50).save()
+        self.assertEqual(Ticket.query.count(), 1)
+        self.assertEqual(Ticket.query.first().name, 'Teste')

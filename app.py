67468347c31c2d7cc306bfde8072e4ac57676198
rodrigondec <rodrigondec@gmail.com>
2019-11-06 16:00:55 -0300

    api root with swagger

--- app.py
+++ app.py
@@ -40,7 +40,23 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 migrate = Migrate(app, db)
 jwt = JWTManager(app)
-swagger = Swagger(app)
+swagger = Swagger(app, config={
+        "headers": [
+        ],
+        "specs": [
+            {
+                "endpoint": 'apispec_1',
+                "route": '/apispec_1.json',
+                "rule_filter": lambda rule: True,  # all in
+                "model_filter": lambda tag: True,  # all in
+            }
+        ],
+        "static_url_path": "/flasgger_static",
+        # "static_folder": "static",  # must be set by user
+        "swagger_ui": True,
+        "specs_route": "/"
+    }
+)
 
 
 add_resources(api)

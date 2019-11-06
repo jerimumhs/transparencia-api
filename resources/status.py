a21875b51353312a621576510168b6c20d5680cd
rodrigondec <rodrigondec@gmail.com>
2019-11-06 16:01:05 -0300

    status documentation

--- resources/status.py
+++ resources/status.py
@@ -5,4 +5,13 @@ from loguru import logger
 class ServerStatus(Resource):
     @logger.catch
     def get(self):
+        """
+        get endpoint
+        ---
+        tags:
+          - Status
+        responses:
+          200:
+            description: The api is up
+        """
         return {'status': 'I\'m fine, thanks.'}, 200

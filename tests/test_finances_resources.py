ea48b3861ada159c7d5f17b3216551da2ebfa401
rodrigondec <rodrigondec@gmail.com>
2019-11-08 17:50:10 -0300

    test resources

---
+++ tests/test_finances_resources.py
@@ -0,0 +1,50 @@
+import datetime
+
+from marshmallow.exceptions import ValidationError
+
+from tests import BaseAPITestCase, BaseTestCase
+from models.finances import Ticket
+from schemas.finances import ticket_schema
+from tests.factories.finances import TicketFactory
+
+
+class TestTicketResources(BaseTestCase):
+    # def setUp(self):
+    #     super(TestTicketResources, self).setUp()
+    #     self.endpoint = 'tickets'
+
+    def test_create_ticket(self):
+        self.assertEqual(Ticket.query.count(), 0)
+
+        data = {
+            'title': 'teste',
+            'value': 10.0,
+            'date': '2019-01-01',
+            'type': Ticket.TicketType.INCOME.value
+        }
+
+        # path = self.get_path()
+        response = self.client.post('tickets/', json=data)
+
+        self.assertEqual(response.status_code, 200, msg=response.data)
+        self.assertEqual(Ticket.query.count(), 1)
+
+    def test_create_ticket_fail(self):
+        self.assertEqual(Ticket.query.count(), 0)
+
+        data = {
+            'title': 1,
+            'value': 'a',
+            'date': '201-01',
+            'type': 'a'
+        }
+
+        # path = self.get_path()
+        response = self.client.post('tickets/', json=data)
+
+        self.assertEqual(response.status_code, 400, msg=response.data)
+        self.assertTrue(response.json.get('title'))
+        self.assertTrue(response.json.get('value'))
+        self.assertTrue(response.json.get('date'))
+        self.assertTrue(response.json.get('type'))
+        self.assertEqual(Ticket.query.count(), 0)

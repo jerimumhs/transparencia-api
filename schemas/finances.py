bb478af684069a8fdf9a257db6084829b9824ebb
rodrigondec <rodrigondec@gmail.com>
2019-11-08 17:48:47 -0300

    schema with validation

--- schemas/finances.py
+++ schemas/finances.py
@@ -1,13 +1,24 @@
-from marshmallow import fields
+from marshmallow import fields, validates, ValidationError
 
 from app import ma, db
 from models.finances import Ticket
 
 
 class TicketSchema(ma.ModelSchema):
-    type_name = fields.String(attribute='_type.name', dump_only=True)
-
     class Meta:
         model = Ticket
         session = db.session
         fields = ['title', 'description', 'date', 'value', 'type', 'type_name']
+
+    type_name = fields.String(attribute='_type.name', dump_only=True)
+
+    @validates("type")
+    def validate_type(self, value):
+        try:
+            Ticket.TicketType(value)
+        except ValueError as e:
+            raise ValidationError(f'{e}') from e
+
+
+ticket_schema = TicketSchema()
+tickets_schema = TicketSchema(many=True)
